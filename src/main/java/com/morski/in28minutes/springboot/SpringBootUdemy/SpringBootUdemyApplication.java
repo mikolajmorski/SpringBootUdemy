a37268137d72f01c5f8fd9f8c4ed9a96742e90a3
mmors <m.morski@gmail.com>
2023-10-28 21:42:44 +0200

    Master Java Web Services and REST API with Spring Boot
    29.Setting up New Spring Boot Project with Spring Initializr

---
+++ src/main/java/com/morski/in28minutes/springboot/SpringBootUdemy/SpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.morski.in28minutes.springboot.SpringBootUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morski/in28minutes/springboot/SpringBootUdemy/SpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morski.in28minutes.springboot.SpringBootUdemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
